dfc12ccdbca642b6029ea2e0381301f896ac9724
Brian Feaver <brian.feaver@gmail.com>
2013-04-10 21:25:52 -0700

    Initial project.

---
+++ src/Acme/DemoBundle/Entity/Contact.php
@@ -0,0 +1,71 @@
+<?php
+namespace Acme\DemoBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="contact")
+ */
+class Contact
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     * @var string
+     */
+    protected $email;
+
+    /**
+     * @ORM\Column(type="text")
+     * @var string
+     */
+    protected $message;
+
+    /**
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $message
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+}
---
+++ src/SfPres/Bundle/BlogBundle/SfPresBlogBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SfPres\Bundle\BlogBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SfPresBlogBundle extends Bundle
+{
+}
